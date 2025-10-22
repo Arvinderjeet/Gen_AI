942179377f3d8d7328f2009c6a13b51fdc8ae69c
Arvinderjeet Singh <arvinderjeet4@gmail.com>
2025-10-22 20:07:50 +0530

    model using HF

---
+++ Model_using_HF.py
@@ -0,0 +1,29 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+import dotenv
+import os
+
+dotenv.load_dotenv()
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+model_name = "microsoft/Phi-3-mini-4k-instruct"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=HF_TOKEN)
+
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    use_auth_token=HF_TOKEN,
+    torch_dtype=torch.float16,
+    device_map="cuda"
+)
+
+prompt = "Explain recursion in simple terms."
+inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+outputs = model.generate(**inputs, max_new_tokens=100)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+prompt = "what is apple."
+inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+outputs = model.generate(**inputs, max_new_tokens=100)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
